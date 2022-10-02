aacb8bf331d1cb6d21dd9c239ddd9a4b7b7f5121
Ayushi Mohan <mishimohan16@gmail.com>
2022-10-03 00:49:31 +0530

    Scope Annotation

--- annotations/src/main/java/com/ayushi/TennisCoach.java
+++ annotations/src/main/java/com/ayushi/TennisCoach.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
@@ -14,6 +18,18 @@ public class TennisCoach implements Coach {
     //define a default constructor
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    //define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of DoMyCleanupStuff()");
     }
 
     /*
